e131dd2ccf432c636bf8566408a33a2ca3be9742
lilvinz <lilvinz@nscit.com>
2012-10-19 21:43:08 +0200

    discoveryf3: fixed usart3_dsm_cfg

--- flight/board_hw_defs/discoveryf3/board_hw_defs.c
+++ flight/board_hw_defs/discoveryf3/board_hw_defs.c
@@ -427,7 +427,7 @@ static const struct pios_usart_cfg pios_usart3_dsm_cfg = {
 	},
 	.irq = {
 		.init = {
-			.NVIC_IRQChannel = USART2_IRQn,
+			.NVIC_IRQChannel = USART3_IRQn,
 			.NVIC_IRQChannelPreemptionPriority = PIOS_IRQ_PRIO_MID,
 			.NVIC_IRQChannelSubPriority = 0,
 			.NVIC_IRQChannelCmd = ENABLE,
